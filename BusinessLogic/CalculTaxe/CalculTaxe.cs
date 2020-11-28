23dc71f52c88c94206ef2c93f30118ee2ec57613
iosip <iosipoi@gmail.com>
2020-11-28 23:35:37 +0200

    Move db to somee.com

---
+++ BusinessLogic/CalculTaxe/CalculTaxe.cs
@@ -0,0 +1,21 @@
+﻿namespace BusinessLogic.CalculTaxe
+{
+    public class CalculTaxe
+    {
+        public float Pret(float cost, float tva, float reducere) 
+        {
+            float result = 0;
+            result = cost + cost * tva / 100 - cost * reducere / 100;
+
+            return result;
+        }
+
+        public float Pret(float cost, float tva, float reducere, float acciz)
+        {
+            float result = 0;
+            result = cost + cost * tva / 100 - cost * reducere / 100 + cost * acciz / 100;
+
+            return result;
+        }
+    }
+}
---
+++ MagazinCore/Models/ProdusViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagazinCore.Models
+{
+    public class ProdusViewModel
+    {
+        public Produs Produs { get; set; }
+        public float Pret { get; set; }
+    }
+}
