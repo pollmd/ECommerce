91468987eeef5630fe3d7b3d7f864de881917a60
iosip <iosipoi@gmail.com>
2022-07-25 19:29:27 +0300

    Add NewsLetterJob

---
+++ NewsLetterJob/BL/DbManager.cs
@@ -0,0 +1,75 @@
+﻿using Npgsql;
+
+namespace NewsLetterJob.BL
+{
+    public class DbManager
+    {
+        NpgsqlConnection dbconn()
+        {
+            string cs = "Host=abul.db.elephantsql.com;Port=5432;Database=odkihpmm;Username=odkihpmm;Password=aQO6Uxbk6XXfxq46E9PfYBi-HWn_a6mP;";
+            using var con = new NpgsqlConnection(cs);
+
+            return con;
+        }
+
+        public string GetMessageBody()
+        {
+            try
+            {
+                string cs = "Host=abul.db.elephantsql.com;Port=5432;Database=odkihpmm;Username=odkihpmm;Password=aQO6Uxbk6XXfxq46E9PfYBi-HWn_a6mP;";
+                using var con = new NpgsqlConnection(cs);
+                con.Open();
+
+                string sql = "SELECT id, \"Denumire\", \"Cost\", \"Tva\", \"Acciz\", \"Marime\", \"Reducere\", \"Culoare\" FROM public.\"Produs\" where \"Reducere\" > 0 and \"EndReducere\" > now() and \"StartReducere\" < now()";
+
+                using var cmd = new NpgsqlCommand(sql, con);  
+                using var reader = cmd.ExecuteReader();
+
+                var result = "";
+
+                while (reader.Read())
+                {
+                    var pret = reader.GetDouble(2) + reader.GetDouble(3) * reader.GetDouble(2) / 100 + reader.GetDouble(4) * reader.GetDouble(2) / 100;
+                    result += @$"<h1>{reader.GetString(1)}</h1>
+                       <span style='color:green; margin-right:10px'>-{reader.GetDouble(6)}%</span>
+                       <span style='color:red'>pret {pret} lei</span><br>";
+                }
+
+                return result;
+            }
+            catch
+            {
+                throw new Exception();
+            } 
+        }
+
+
+        public List<string> GetEmailsList()
+        {
+            try
+            {
+                string cs = "Host=abul.db.elephantsql.com;Port=5432;Database=odkihpmm;Username=odkihpmm;Password=aQO6Uxbk6XXfxq46E9PfYBi-HWn_a6mP;";
+                using var con = new NpgsqlConnection(cs);
+                con.Open();
+
+                string sql = "SELECT \"Email\" FROM public.\"AspNetUsers\"";
+
+                using var cmd = new NpgsqlCommand(sql, con);
+                using var reader = cmd.ExecuteReader();
+
+                var result = new List<string>();
+
+                while (reader.Read())
+                {
+                    result.Add(reader.GetString(0));
+                }
+
+                return result;
+            }
+            catch
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ NewsLetterJob/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NewsLetterJob.BL;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+
+Console.WriteLine("NewsLetter START!");
+
+sendMail();
+
+void sendMail()
+{
+    Console.WriteLine("====> Trimitere mail ====>");
+
+    var smtpClient = new SmtpClient("smtp.freesmtpservers.com") 
+    {
+        Port=25,
+        //Credentials=new NetworkCredential("iosipoi@gmail.com", "xxxxxx"),
+        //EnableSsl=false,
+    };
+
+    var bd = new DbManager();
+
+    var mailMessage = new MailMessage
+    {
+        From = new MailAddress("admin@emagazin.com"),
+        Subject = "Produse la promotie",
+        Body = bd.GetMessageBody(),
+        IsBodyHtml = true,
+    };
+
+    var emailsList = bd.GetEmailsList();
+    foreach (var email in emailsList)
+    {
+        mailMessage.To.Add(email);
+    }
+
+    smtpClient.Send(mailMessage);   
+}
+
+
